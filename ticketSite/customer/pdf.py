14d846ae9a04f92706247cb6c40b86e23463964b
4470056464 <mhmdsadqd@gmail.com>
2020-02-11 14:40:23 +0330

    initiaol pdf

--- ticketSite/customer/pdf.py
+++ ticketSite/customer/pdf.py
@@ -1,25 +1,9 @@
-import os
-from io import StringIO, BytesIO
-from django.conf import settings
-from xhtml2pdf import pisa
-from django.template.loader import get_template
-from django.template import Context
-from django.http import HttpResponse
-from cgi import escape
+import pdfkit
+config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+# pdfkit.from_string(index.html, 'MyPDF.pdf', configuration=config)
+
+# pdfkit.from_string('hello','string.pdf')
 
 
-def render_to_pdf(template_src, context_dict):
-    template = get_template(template_src)
-    # context = Context(context_dict)
-    context=context_dict
-    html  = template.render(context)
-    result = BytesIO()
-
-    pdf = pisa.pisaDocument(BytesIO(html.encode('utf-8')), result,link_callback=fetch_resources,encoding='utf-16')
-    if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
-    return None
-    # return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
-def fetch_resources(uri,rel):
-    path= os.path.join(settings.MEDIA_ROOT,uri.replace(settings.MEDIA_URL,""))
-    return path
+pdfkit.from_file('index.html','file.pdf',configuration=config)
+# pdfkit.from_url("http://google.com", "out.pdf")
