64bc09422857473c48448b22b20fd2b82642eb3d
4470056464 <mhmdsadqd@gmail.com>
2020-03-01 09:20:50 +0330

    ass header

--- ticketSite/cart/urls.py
+++ ticketSite/cart/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
   path('', views.cart_detail,name='cart_detail'),
   path('add/<int:product_id>', views.cart_add,name='cart_add'),
   # path('reserve/', views.reserve_ticket,name='reserve_ticket')
-  # path('remove/<int:product_id>', views.cart_remove,name='cart_remove'),
+  path('remove/<int:product_id>', views.cart_remove,name='cart_remove'),
 ]
--- ticketSite/cart/views.py
+++ ticketSite/cart/views.py
@@ -55,11 +55,11 @@ def cart_add(request, product_id):
     return redirect('cart_detail')
 
 
-# def cart_remove(request, product_id):
-#     cart = Cart(request)
-#     product = get_object_or_404(Product, id=product_id)
-#     cart.remove(product)
-#     return redirect('cart_detail')
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart_detail')
 
 
 def cart_detail(request):
